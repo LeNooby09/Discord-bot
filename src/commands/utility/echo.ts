34d8fa50e268a3c908bb08949462bd4a0c51ccb0
bytexenon <bytexenon@proton.me>
2025-01-15 12:34:55 -0700

    feat(commands): add echo command to repeat user input

---
+++ src/commands/utility/echo.ts
@@ -0,0 +1,17 @@
+import { SlashCommandBuilder } from "discord.js";
+import { CommandContext } from "../../types/CommandContext";
+
+export const data = new SlashCommandBuilder()
+  .setName("echo")
+  .setDescription("Echoes your input")
+  .addStringOption((option) =>
+    option
+      .setName("input")
+      .setDescription("The input to echo")
+      .setRequired(true)
+  );
+
+export async function execute(context: CommandContext) {
+  const input = context.interaction.options.getString("input")!;
+  await context.interaction.channel?.send(input);
+}
