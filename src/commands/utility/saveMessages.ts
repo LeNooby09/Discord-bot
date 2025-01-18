591a86682e103fbdf523e52b0bf5043ca4f174b3
bytexenon <bytexenon@proton.me>
2025-01-18 08:39:46 -0700

    refactor(saveMessages): streamline message saving process and improve filename handling

--- src/commands/utility/saveMessages.ts
+++ src/commands/utility/saveMessages.ts
@@ -1,28 +1,15 @@
 // Imports //
-import fs from "fs";
-import zlib from "zlib";
-import * as logger from "../../logger.js";
-
 import {
-  PermissionFlagsBits,
+  AttachmentBuilder,
   SlashCommandBuilder,
   TextChannel,
 } from "discord.js";
 import { CommandContext } from "../../types/CommandContext";
 
 // Constants //
-const TMP_DIRECTORY = "/tmp/DiscordBot/";
-const DEFAULT_FILENAME = "messages";
-const FILENAME_POSTFIX = ".json.gz";
-const VALID_FILENAME_REGEX = /^[a-zA-Z0-9-_]+$/;
+const JSON_FILENAME = "messages-{CHANNEL_ID}.json";
 
 // Local Functions //
-function createTempDirectoryIfNotExists() {
-  if (!fs.existsSync(TMP_DIRECTORY)) {
-    fs.mkdirSync(TMP_DIRECTORY);
-    logger.info("Created temporary directory for saving messages.");
-  }
-}
 async function fetchAllMessages(channel: TextChannel, interactionId: string) {
   const messages = [];
   let messageIdCursor = interactionId;
@@ -49,96 +36,48 @@ async function fetchAllMessages(channel: TextChannel, interactionId: string) {
 
   return messages;
 }
-function jsonAndGzipMessages(messages: any[]) {
-  const messagesString = JSON.stringify(messages);
-  const messagesBuffer = Buffer.from(messagesString, "utf-8");
-  const compressed = zlib.gzipSync(messagesBuffer);
-
-  logger.info(
-    `Compressed messages from ${messagesBuffer.length} bytes to ${compressed.length} bytes`
-  );
-
-  return compressed;
-}
-function cleanUpFile(filePath: string) {
-  if (fs.existsSync(filePath)) {
-    fs.unlinkSync(filePath);
-    logger.info(`Deleted temporary file ${filePath}`);
-  } else {
-    logger.error(`Failed to delete non-existent temporary file ${filePath}`);
-  }
+function formatString(template: string, replacements: Record<string, string>) {
+  return template.replace(/{(.*?)}/g, (_match, key) => replacements[key] ?? "");
 }
 
 // Exports //
 export const data = new SlashCommandBuilder()
   .setName("save-messages")
-  .setDescription("Saves all messages in the channel to a JSON file")
-  .addChannelOption((option) =>
-    option
-      .setName("channel")
-      .setDescription("The channel to save messages from")
-      .setRequired(false)
-  )
-  .addStringOption((option) =>
-    option
-      .setName("filename")
-      .setDescription("The name of the file to save to")
-      .setRequired(false)
-  );
+  .setDescription("Saves all messages in the channel to a JSON file");
 
 export async function execute(context: CommandContext) {
-  createTempDirectoryIfNotExists();
-  const channel =
-    context.interaction.options.getChannel("channel") ??
-    context.interaction.channel;
-  const filename =
-    context.interaction.options.getString("filename") ?? DEFAULT_FILENAME;
-
+  const channel = context.interaction.channel;
   if (!(channel instanceof TextChannel)) {
     await context.interaction.reply({
       content: "This command only works in text channels",
       flags: "Ephemeral",
     });
     return;
-  } else if (!filename.match(VALID_FILENAME_REGEX)) {
-    await context.interaction.reply({
-      content:
-        "Invalid filename. Only alphanumeric characters, hyphens, and underscores are allowed.",
-      flags: "Ephemeral",
-    });
-    return;
-  } else if (
-    !channel
-      .permissionsFor(context.interaction.user)
-      .has(PermissionFlagsBits.ViewChannel)
-  ) {
-    await context.interaction.reply({
-      content: "You do not have permission to view messages in this channel",
-      flags: "Ephemeral",
-    });
-    return;
   }
 
+  // Acknowledge the interaction
+  await context.interaction.deferReply();
+
+  // Fetch all messages in the channel
   const messages = await fetchAllMessages(channel, context.interaction.id);
+  const messagesJSON = JSON.stringify(messages);
 
-  // Save messages to file
-  const realFilePath = TMP_DIRECTORY + filename + FILENAME_POSTFIX;
-  const gzippedMessages = jsonAndGzipMessages(messages);
-  fs.writeFileSync(realFilePath, gzippedMessages);
-  logger.info(`Saved temporary file with messages to ${realFilePath}`);
+  // Create an attachment with the JSON data of the messages
+  const formatReplacements = {
+    CHANNEL_ID: context.interaction.channelId,
+    INTERACTION_ID: context.interaction.id,
+  };
+  const fileName = formatString(JSON_FILENAME, formatReplacements);
+  const savedMessagesAttachment = new AttachmentBuilder(
+    Buffer.from(messagesJSON, "utf-8"),
+    {
+      name: fileName,
+    }
+  );
 
-  await context.interaction.reply({
-    content: `Saved messages to ${filename}.gz (${messages.length} messages)`,
-    files: [
-      {
-        attachment: realFilePath,
-        // For some fucking reason whenever the filename is "<filename>.json.gz" Discord
-        // Converts the filename to "<filename>.json.json" when downloading it. So
-        // We're just going to remove the ".json" part of the filename.
-        name: filename + ".gz",
-      },
-    ],
+  // Edit the "Bot is thinking" message with the saved messages
+  await context.interaction.editReply({
+    content: `Saved messages (${messages.length} messages)`,
+    files: [savedMessagesAttachment],
   });
-
-  cleanUpFile(realFilePath);
 }
